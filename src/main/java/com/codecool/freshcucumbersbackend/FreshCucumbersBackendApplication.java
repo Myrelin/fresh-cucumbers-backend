50a4ee42fc71a18f2a9ba01c394264dc5ab85b05
myrelin <anna.iklody@gmail.com>
2019-03-19 13:58:28 +0100

    initial commit

---
+++ src/main/java/com/codecool/freshcucumbersbackend/FreshCucumbersBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.freshcucumbersbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshCucumbersBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreshCucumbersBackendApplication.class, args);
+    }
+
+}
